02bd03f663ceac3313fd84c83dabf44bb6044ef8
inaciovieiraa <contato.inaciovieira@outlook.com>
2026-02-06 11:33:06 -0300

    Atualizando script visual e adicionando gráfico demográfico

--- 03_visualizacao.R
+++ 03_visualizacao.R
@@ -1,7 +1,36 @@
 library(tidyverse)
 library(ggplot2)
+source("02_analise_esavi.R")
 
-#criando um gráfico de barras que nos mostra a taxa de reação percentual de cada vacina por fabricante
+#===========================================================================================
+# 1. ANÁLISE DEMOGRÁFICA POR SEXO:
+#===========================================================================================
+
+#Extraindo e separando dados por sexo:
+
+dados_sexo <- base_limpa %>% 
+  count(SEXO) %>% 
+  mutate(porcentagem = n / sum(n) * 100)
+
+#Criando um gráfico de pizza:
+
+grafico_sexo <- dados_sexo %>% 
+  ggplot(aes(x = "", y = porcentagem, fill = SEXO)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  theme_void() +
+  geom_text(aes(label = sprintf("%.1f%%", porcentagem)),
+            position = position_stack(vjust = 0.5)) +
+  labs(title = "DISTRIBUIÇÃO POR SEXO", fill = "SEXO") +
+  scale_fill_manual(values = c("#FC0FC0", "#87cefa")) 
+
+ggsave("grafico_sexo.png", plot = grafico_sexo, width = 6, height = 6)
+
+#===========================================================================================
+# 2. RESULTADOS OBTIDOS (TAXA DE REAÇÃO):
+#===========================================================================================
+
+#criando um gráfico de barras que nos mostra a taxa de reação percentual de cada vacina por fabricante:
 
 grafico_barras <- relatorio_final %>% 
   ggplot(aes(x = reorder(FABRICANTE_REAL, TAXA_REACAO_PERCENTUAL), y = TAXA_REACAO_PERCENTUAL)) +
@@ -16,5 +45,3 @@ grafico_barras <- relatorio_final %>%
   theme_classic() 
 
 ggsave("grafico_taxa_reacao.png", plot = grafico_barras, width = 8, height = 5)
-    
-    
